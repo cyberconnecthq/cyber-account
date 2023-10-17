6d770d734fb02217c5fd7e174b00cc3b5fb3b99b
Wei Su <me@weisu.io>
2023-10-17 12:57:00 +0800

    feat: add batch transaction (#3)

--- src/ABIs/index.ts
+++ src/ABIs/index.ts
@@ -2,3 +2,4 @@ export { KernelAccountAbi } from "./KernelAccountAbi";
 export { KernelFactoryAbi } from "./KernelFactoryAbi";
 export { EntryPointAbi } from "./EntryPointAbi";
 export { TokenReceiverAbi } from "./TokenReceiverAbi";
+export { MultiSendAbi } from "./MultiSendAbi";
